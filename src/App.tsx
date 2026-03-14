d396988b622d7ef131303dcff2351e5774ef6784
Rodion Barkov <rodion_barkov@mail.ru>
2026-03-14 22:51:46 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,28 @@
 
 function App() {
-  
+
   const tracks = [
-    { title: 'Song', url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3'},  
-    { title: 'Instrumental', url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3'},
-    { title: 'Song', url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3'},  
-    { title: 'Instrumental', url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3'},
+    { id: 1, title: 'Song', url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3' },
+    { id: 2, title: 'Instrumental', url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3' },
+    { id: 3, title: 'Song', url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3' },
+    { id: 4, title: 'Instrumental', url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3' },
   ]
 
-  const divTrack = tracks.map ((track) => {
-    return (
-      <div className="main">
-      <h1>{track.title}</h1>
-      <audio src={track.url} controls></audio>
-    </div>
-    )
-  })
+
 
   return (
     <>
-  
-{divTrack}
+
+      {tracks.map((track) => {
+        return (
+          <div key={track.id}className="main">
+            <h1>{track.title}</h1>
+            <audio src={track.url} controls></audio>
+          </div>
+        )
+      })}
     </>
   )
 }
 
 export default App
- 
